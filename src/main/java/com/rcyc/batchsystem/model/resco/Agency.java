c28af374cc1b11d1b3437efdcb9505d6efa85376
Nivetha Manoharan <Nivetha.Manoharan@ritz-carltonyachtcollection.com>
2025-07-21 12:30:46 +0530

    JAXB changes

--- src/main/java/com/rcyc/batchsystem/model/resco/Agency.java
+++ src/main/java/com/rcyc/batchsystem/model/resco/Agency.java
@@ -5,8 +5,11 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 @XmlRootElement(name = "Agency")
 @XmlAccessorType(XmlAccessType.FIELD)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Agency {
     @XmlElement(name = "AgentId")
     private String agentId;
